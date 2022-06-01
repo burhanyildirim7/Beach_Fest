9f63e7df0a78eb6cfe01f99bafa4488eb1b6a2c3
Kuntay KOCA <kuntay.koca.development@gmail.com>
2022-06-02 01:49:13 +0300

    script düzenlendi

--- Assets/Scripts/Kuntay/Sezlong2inciGrubuAcma.cs
+++ Assets/Scripts/Kuntay/Sezlong2inciGrubuAcma.cs
@@ -8,19 +8,39 @@ public class Sezlong2inciGrubuAcma : MonoBehaviour
 {
     [SerializeField] Text bedelText;
     [SerializeField] GameObject acilacakSezlongGrubu;
-    bool isOpen=0;
+    [SerializeField] GameObject kapanacakGrubu;
+    int isOpen=0;
     // Start is called before the first frame update
     void Start()
     {
         isOpen = PlayerPrefs.GetInt("2inciSezlongAcikMi");
+
+        if (isOpen == 1)
+        {
+            isOpen = 1;
+            PlayerPrefs.SetInt("2inciSezlongAcikMi", 1);
+            acilacakSezlongGrubu.SetActive(true);
+            kapanacakGrubu.SetActive(false);
+        }
+        else
+        {
+
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (bedelText.text=="$0"||isOpen==1)
+        if (bedelText.text=="$0" && isOpen==0)
         {
+            isOpen = 1;
+            PlayerPrefs.SetInt("2inciSezlongAcikMi",1);
             acilacakSezlongGrubu.SetActive(true);
+            kapanacakGrubu.SetActive(false);
+        }
+        else
+        {
+                
         }
     }
 }
