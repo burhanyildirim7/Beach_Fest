a7390caa71f3f8b970efcd07c3013f925da8ecab
Kuntay KOCA <kuntay.koca.development@gmail.com>
2022-06-13 01:35:20 +0300

    rev devamı

--- Assets/Scripts/Kuntay/garsonAcmaScripti.cs
+++ Assets/Scripts/Kuntay/garsonAcmaScripti.cs
@@ -11,12 +11,35 @@ public class garsonAcmaScripti : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        garsonSayisi = PlayerPrefs.GetInt("GarsonSayisi");
+        if (garsonSayisi == 1)
+        {
+            garson1Object.SetActive(true);
+        }
+        else if (garsonSayisi == 2)
+        {
+            garson2Object.SetActive(true);
+        }
+        else
+        {
+
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (garsonSayisi == 1)
+        {
+            garson1Object.SetActive(true);
+        }
+        else if (garsonSayisi == 2)
+        {
+            garson2Object.SetActive(true);
+        }
+        else
+        {
+
+        }
     }
 }
